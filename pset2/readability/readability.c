3bed6cb9124fec62978b8247b72a5ef994da8fe2
Muhammad Yousif <m.alshf3ee@gmail.com>
2021-09-21 11:02:45 +0000

    Lab2: scrabble

---
+++ pset2/readability/readability.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(void)
+{
+    // Get input words from both players
+    string paragraph = get_string("Paragraph: ");
+
+}
