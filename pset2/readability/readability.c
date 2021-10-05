514c0cd1a0ce06cf3084e9997af60621c47a72a0
Muhammad Yousif <m.alshf3ee@gmail.com>
2021-10-05 18:07:53 +0000

    Fix: Rounding Bug

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -5,53 +5,62 @@
 #include <math.h>
 
 
-int main() {
+int main()
+{
+    // read text
     string s = get_string("Input:  ");
 
-
+    // set vars
     int length = strlen(s);
     int words = 0;
     int sentences = 0;
     int letters = 0;
 
 
-    for(int i = 0; i < length; i++ )
+    // loop over letters
+    for (int i = 0; i < length; i++)
     {
-        if(s[i] == ' ')
-        {
-            words++;
-        }
-
-        if((s[i] > 65 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+        // inspect letters
+        if (isalpha(s[i]))
         {
             letters++;
         }
 
-        if(s[i] == '.' || s[i] == '!' || s[i] == '?')
+        // inspect words
+        if (isspace(s[i]))
+        {
+            words++;
+        }
+
+        // inspect sentences
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
         {
             sentences++;
         }
 
     }
-    if(length > 0){
+
+    // words must be number of spaces + 1
+    if (length > 0)
+    {
         words += 1;
     }
 
     // average number of sentences per 100 words
-    float S = sentences * 100 / words; // 4 * 100 / 14
+    float S =  (sentences * 100.0 / words); // 4 * 100 / 14
 
     // average number of sentences per 100 words
-    float L = letters * 100 / words;
+    float L =  (letters * 100.0 / words);
 
 
     float index = (0.0588 * L) - (0.296 * S) - (15.8);
 
 
-    if( index < 1)
+    if(index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(index > 16)
+    else if (index > 16)
     {
         printf("Grade 16+\n");
     }
