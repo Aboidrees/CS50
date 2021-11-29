16a96001695a4a87d72e6727f77edf5a352e4cfa
Muhammad Yousif <m.alshf3ee@gmail.com>
2021-11-29 14:53:43 +0300

    Week 9: Fincance $ end

--- pset9/finance/application.py
+++ pset9/finance/application.py
@@ -6,15 +6,18 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-import requests, json
-
 from helpers import apology, login_required, lookup, usd
+from datetime import datetime as dt
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# export API_KEY=
+# export FLASK_APP=application.py
 
 
 # Ensure responses aren't cached
@@ -42,63 +45,89 @@ db = SQL("sqlite:///finance.db")
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
 
-params = {'token': os.environ.get("API_KEY")}
 
 @app.route("/")
-# @login_required
+@login_required
 def index():
-    """Show portfolio of stocks"""
-    purchases = db.execute("SELECT * FROM purchases WHERE user_id=?", 1)
+    # Save the session in variable
+    user_id = session.get('user_id')
 
-    return render_template("index.html", purchases=purchases)
+    operations_records = db.execute(
+        "SELECT id, name, symbol, SUM(price) AS price, SUM(shares) AS shares "
+        "FROM operations "
+        "WHERE user_id=? GROUP BY symbol", user_id
+    )
+
+    user = db.execute("SELECT * FROM users WHERE id=?", user_id)
+
+    operations = []
+    for operation in operations_records:
+        data = lookup(operation['symbol'])
+
+        operation['price'] = data['price']
+        operations.append(operation)
+
+    return render_template("index.html", operations=operations, cash=user[0]['cash'])
 
 
 @app.route("/buy", methods=["GET", "POST"])
 @login_required
 def buy():
-    """Buy shares of stock"""
     if request.method == "POST":
+        # Save the form variable
         symbol = request.form.get('symbol')
         shares = request.form.get('shares')
 
-        if not symbol or not shares:
-            return apology("All fileds are mandetory")
+        # Save the session in variable
+        user_id = session.get('user_id')
 
-        if int(shares) <= 0:
-            return apology("Shares value should be posative number")
+        if not symbol or not shares:
+            return apology("All fields are mandatory")
+
+        if not shares.isdigit() or int(shares) <= 0:
+            return apology("Shares value should be positive number and greater than zero")
 
         data = lookup(symbol)
+        if data is None:
+            return apology("Not Found")
 
-        import time
+        user = db.execute("SELECT * FROM users WHERE id=?", user_id)
+        available_cash = float(user[0]['cash'])
 
-        user_id = session.get('user_id')
-        name = data['name']
-        price = usd(data['price'])
-        symbol = data['symbol']
-
-        _time = time.time()
-
-        result = db.execute(
-            "INSERT INTO purchases (user_id, name, symbol, price, shares, time) VALUES(?, ?, ?, ?, ?, ?)",
-            user_id, name, symbol, price, shares, _time
-        )
-
-        if result:
-            flash("Bought!")
-            return redirect('/')
+        if available_cash >= int(shares) * data['price']:
+            time = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+            record_operation = db.execute(
+                "INSERT INTO operations (user_id, name, symbol, price, shares, type, time) "
+                "VALUES(?, ?, ?, ?, ?, 'buy', ?)",
+                user_id, data['name'], data['symbol'], data['price'], shares, time
+            )
+            if record_operation:
+                new_cash = available_cash - int(shares) * data['price']
+                update_cash = db.execute("UPDATE users SET cash=? WHERE id=?", new_cash, user_id)
+                if update_cash:
+                    flash("Sold!")
+                    return redirect('/')
+                else:
+                    apology("Couldn't update user cash")
+            else:
+                apology("Couldn't register the operation")
         else:
-            flash("Failed!")
-
+            apology('user cannot afford the number of shares at the current price')
     return render_template("buy.html")
-
-    # return apology("TODO")
 
 
 @app.route("/history")
 @login_required
 def history():
-    """Show history of transactions"""
-    return apology("TODO")
+    result = db.execute("SELECT * FROM operations WHERE user_id=?", 1)
+    operations = []
+
+    for operation in result:
+        if operation['type'] == "sel":
+            operation['shares'] *= -1
+        operations.append(operation)
+
+    return render_template("history.html", operations=operations)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -149,7 +178,7 @@ def logout():
 
 
 @app.route("/quote", methods=["GET", "POST"])
-# @login_required
+@login_required
 def quote():
     """Get stock quote."""
     if request.method == "POST":
@@ -158,11 +187,11 @@ def quote():
         if symbol:
             data = lookup(symbol)
             if data is None:
-                return apology("Not Found", 404)
+                return apology("Not Found")
 
             return render_template('quoted.html', data=data)
         else:
-            return apology("Enter vaild symbol")
+            return apology("Enter valid symbol")
 
     return render_template('quote.html')
 
@@ -173,7 +202,7 @@ def register():
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
-        cpassword = request.form.get('cpassword')
+        confirmation = request.form.get('confirmation')
 
         if not username:
             return apology("username field can not be blank")
@@ -186,17 +215,17 @@ def register():
         if not password:
             return apology("password field can not be blank")
 
-        if not cpassword:
+        if not confirmation:
             return apology("password confirmation field can not be blank")
 
-        if password != cpassword:
+        if password != confirmation:
             return apology("passwords do not match")
 
-        password =  generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
         _id = db.execute("INSERT INTO users (username, hash) VALUES (?,?)", username, password)
         if _id:
-            session["user_id"]  = _id
+            session["user_id"] = _id
             return redirect('/')
         else:
             return redirect('/login')
@@ -207,8 +236,77 @@ def register():
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
 def sell():
-    """Sell shares of stock"""
-    return apology("TODO")
+    if request.method == "POST":
+        # Save the form variable
+        symbol = request.form.get('symbol')
+        shares = request.form.get('shares')
+
+        # Save the session in variable
+        user_id = session.get('user_id')
+
+        # check if variable is empty
+        if not symbol or not shares:
+            return apology("All fields are mandatory")
+
+        # check if the shares is numaric
+        if not shares.isdigit() or int(shares) <= 0:
+            return apology("Shares value should be positive number and greater than zero")
+
+        # get stock data
+        data = lookup(symbol)
+
+        # check if the stock data is empty
+        if data is None:
+            return apology("Not Found")
+
+        # get user information to update the cash
+        user = db.execute("SELECT * FROM users WHERE id=?", user_id)
+        available_cash = float(user[0]['cash'])
+
+        # get company held shares information for check and update
+        user_shares = db.execute(
+            "SELECT id, name, symbol, SUM(price) AS price, SUM(shares) AS shares "
+            "FROM operations "
+            "WHERE user_id=? and symbol=? GROUP BY symbol ", user_id, symbol
+        )
+        available_shares = user_shares[0]['shares']
+
+        # check if the available stock shares can satisfy the sell operation
+        if available_shares >= int(shares):
+            time = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+            record_operation = db.execute(
+                "INSERT INTO operations (user_id, name, symbol, price, shares, type, time) "
+                "VALUES(?, ?, ?, ?, ?, 'sell', ?)",
+                user_id, data['name'], data['symbol'], data['price'], (int(shares) * -1), time
+            )
+            # check if the operation is registered
+            if record_operation:
+                new_cash = available_cash + int(shares) * data['price']
+                update_cash = db.execute("UPDATE users SET cash=? WHERE id=?", new_cash, user_id)
+
+                # check if the cash is updated
+                if update_cash:
+
+                    # save flash success and redirect
+                    flash("Sold!")
+                    return redirect('/')
+                else:
+
+                    # apology if cash updated fails
+                    return apology("Couldn't update user cash")
+            else:
+
+                # apology if the operation is not registered
+                return apology("Couldn't register the operation")
+
+        else:
+
+            # apology if no sufficient stock shares
+            return apology('user cannot afford the number of shares at the current price')
+
+    # list of available stock shares
+    held_stocks = db.execute("SELECT * FROM operations GROUP BY symbol HAVING shares>0")
+    return render_template("sell.html", held_stocks=held_stocks)
 
 
 def errorhandler(e):
